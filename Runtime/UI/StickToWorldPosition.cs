1bccc6a173441a7ff936ff93e5f1ffccbaf9eacf
passivestar <60579014+passivestar@users.noreply.github.com>
2022-10-30 22:15:40 +0400

    Add StickToWorldPosition

---
+++ Runtime/UI/StickToWorldPosition.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace EssentialUtils
+{
+    public class StickToWorldPosition : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+        [SerializeField] Camera targetCamera;
+        [SerializeField] Vector3 worldPositionOffset;
+        [SerializeField] bool showOffScreen;
+        [SerializeField] float offScreenMargin = .1f;
+
+        void Start()
+        {
+            if (targetCamera == null)
+            {
+                targetCamera = Camera.main;
+            }
+        }
+
+        void Update()
+        {
+            var targetPosition = target.position + worldPositionOffset;
+            var viewportPoint = targetCamera.WorldToViewportPoint(targetPosition);
+
+            if (viewportPoint.z < 0)
+            {
+                viewportPoint.x = viewportPoint.x < 0.5 ? 2 : -1;
+                viewportPoint.y = 1 - viewportPoint.y;
+            }
+
+            if (showOffScreen)
+            {
+                viewportPoint.x = Mathf.Clamp(viewportPoint.x, offScreenMargin, 1f - offScreenMargin);
+                viewportPoint.y = Mathf.Clamp(viewportPoint.y, offScreenMargin, 1f - offScreenMargin);
+
+                // Will stick to the vertical center of the screen when offscreen
+                viewportPoint.y = Math.MapRange(viewportPoint.z, 0f, 10f, .5f, viewportPoint.y);
+            }
+
+            transform.position = new(viewportPoint.x * Screen.width, viewportPoint.y * Screen.height, 0f);
+        }
+    }
+}
