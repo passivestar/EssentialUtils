3b727c006062e935b70d84d59bf608751c379f9f
passivestar <60579014+passivestar@users.noreply.github.com>
2022-12-02 12:33:47 +0400

    Create EnumerableExtensions.cs

---
+++ Runtime/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+namespace EssentialUtils
+{
+    public static class EnumerableExtensions
+    {
+        public static Tuple<List<T>, List<T>> Partition<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            var listA = new List<T>();
+            var listB = new List<T>();
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    listA.Add(item);
+                }
+                else
+                {
+                    listB.Add(item);
+                }
+            }
+            return new Tuple<List<T>, List<T>>(listA, listB);
+        }
+    }
+}
