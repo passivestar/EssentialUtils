41876af6497bf40bdeee589b52bbc96daa153af8
passivestar <60579014+passivestar@users.noreply.github.com>
2022-12-27 11:25:34 +0400

    Update Oscillator.cs

--- Runtime/Time/Oscillator.cs
+++ Runtime/Time/Oscillator.cs
@@ -1,20 +1,41 @@
-using System;
 using UnityEngine;
 
 namespace EssentialUtils
 {
     public class Oscillator
     {
+        public float Amplitude { get; set; }
+        public float Frequency { get; set; }
+        public float Offset { get; set; }
+        public float ValueOffset { get; set; }
+        public bool UnscaledTime { get; set; }
+
+        public float Value { get; private set; }
+
         float elapsed;
 
-        // TODO: Subscribe to update, no need to call manually
-
-        public float Run(float amplitude = 1f, float frequency = 1f, float offset = 0f,
+        public Oscillator(float amplitude = 1f, float frequency = 1f, float offset = 0f,
             float valueOffset = 0f, bool unscaledTime = false)
         {
-            elapsed += unscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
-            var valueNormalized = Mathf.Sin(elapsed * frequency * Mathf.PI + offset * Mathf.PI);
-            return valueNormalized * amplitude + valueOffset;
+            Amplitude = amplitude;
+            Frequency = frequency;
+            Offset = offset;
+            ValueOffset = valueOffset;
+            UnscaledTime = unscaledTime;
+
+            MonoBehaviourHelper.Instance.onUpdate += Update;
+        }
+
+        public void Dispose()
+        {
+            MonoBehaviourHelper.Instance.onUpdate -= Update;
+        }
+
+        public void Update()
+        {
+            elapsed += UnscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
+            var valueNormalized = Mathf.Sin(elapsed * Frequency * Mathf.PI + Offset * Mathf.PI);
+            Value = valueNormalized * Amplitude + ValueOffset;
         }
     }
 }
